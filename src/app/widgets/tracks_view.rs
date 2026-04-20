8d28f47c5a9356ba374bf06a53bf68cb9f3ddfa5
sevonj <100710152+sevonj@users.noreply.github.com>
2026-04-20 22:59:50 +0300

    feat: vertical zoom

--- src/app/widgets/tracks_view.rs
+++ src/app/widgets/tracks_view.rs
@@ -22,6 +22,60 @@ use crate::app::widgets::TrackView;
 
 const TOP_HEIGHT: f32 = 24.0;
 const TRACK_HEIGHT: f32 = 96.0;
+const DEFAULT_ZOOM: f32 = 8.0;
+
+#[derive(Debug, Clone)]
+struct ViewPortState {
+    time_zoom: f32,
+    time_off: f32,
+    scroll_off: f32,
+    track_height: f32,
+}
+
+impl Default for ViewPortState {
+    fn default() -> Self {
+        Self {
+            time_zoom: DEFAULT_ZOOM,
+            time_off: 0.0,
+            scroll_off: 0.0,
+            track_height: TRACK_HEIGHT,
+        }
+    }
+}
+
+impl ViewPortState {
+    pub fn horizontal_zoom(
+        &mut self,
+        delta: f32,
+        rect: Rect,
+        relative_cursor_pos: Vec2,
+        time_scale: f32,
+    ) {
+        let old_zoom = self.time_zoom;
+        let mut new_zoom = old_zoom;
+        if delta > 0.0 {
+            new_zoom *= 1.0 + delta * 0.2;
+        } else {
+            new_zoom /= 1.0 - delta * 0.2;
+        }
+        self.time_zoom = new_zoom;
+        let old_len = rect.width() / old_zoom;
+        let new_len = rect.width() / new_zoom;
+        let delta_len = (new_len - old_len) * time_scale;
+        self.time_off -= delta_len * relative_cursor_pos.x / rect.width();
+    }
+
+    pub fn vertical_zoom(&mut self, delta: f32, num_tracks: usize) {
+        let old_height = TOP_HEIGHT + num_tracks as f32 * self.track_height;
+        if delta > 0.0 {
+            self.track_height *= 1.0 + delta * 0.1;
+        } else {
+            self.track_height /= 1.0 - delta * 0.1;
+        }
+        let new_height = TOP_HEIGHT + num_tracks as f32 * self.track_height;
+        self.scroll_off *= new_height / old_height;
+    }
+}
 
 pub struct TracksView<'a> {
     session: &'a mut Session,
@@ -37,24 +91,14 @@ impl Widget for TracksView<'_> {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
         let length = self.session.length();
 
-        let state_id = ui.id().with("zoom");
-        const DEFAULT_ZOOM: f32 = 8.0;
-        let (mut zoom, mut offset) = ui.data_mut(|d| {
-            d.get_temp::<(f32, Vec2)>(state_id)
-                .unwrap_or((DEFAULT_ZOOM, Vec2::splat(0.0)))
-        });
+        let state_id = ui.id().with("tracks_viewport_state");
+        let mut state = ui.data_mut(|d| d.get_temp::<ViewPortState>(state_id).unwrap_or_default());
         let midi_time_scale = match &self.session.midi_header().division {
             midi_msg::Division::TicksPerQuarterNote(ticks) => *ticks as f32,
             midi_msg::Division::TimeCode {
                 ticks_per_frame, ..
             } => *ticks_per_frame as f32,
         };
-
-        let scroll_offset_id = ui.id().with("scroll_offset");
-        let mut scroll_offset = ui.data_mut(|d| {
-            d.get_temp::<Vec2>(scroll_offset_id)
-                .unwrap_or(Vec2::splat(0.0))
-        });
 
         let style = ui.global_style();
         let weak_bg_fill = style.visuals.widgets.open.weak_bg_fill;
@@ -76,35 +120,34 @@ impl Widget for TracksView<'_> {
                             ui.style_mut().spacing.item_spacing = item_spacing;
 
                             if ui.button("Reset Zoom").clicked() {
-                                zoom = DEFAULT_ZOOM;
+                                state.time_zoom = DEFAULT_ZOOM;
                             }
                             if ui.button("Reset Position").clicked() {
-                                offset.x = 0.0;
+                                state.time_off = 0.0;
                             }
                         });
                     });
 
                 let scroll_resp = ScrollArea::vertical()
                     .scroll_bar_visibility(ScrollBarVisibility::AlwaysHidden)
-                    .scroll_offset(scroll_offset)
+                    .scroll_offset(vec2(0.0, state.scroll_off))
                     .show(ui, |ui| {
                         ui.vertical(|ui| {
                             for (index, track) in self.session.tracks_mut().iter_mut().enumerate() {
                                 ui.scope(|ui| {
-                                    ui.set_height(TRACK_HEIGHT);
+                                    ui.set_height(state.track_height);
                                     ui.style_mut().spacing.item_spacing = item_spacing;
                                     ui.add(TrackView::new(index, track));
                                 });
                             }
                         })
                     });
-                scroll_offset = scroll_resp.state.offset;
+                state.scroll_off = scroll_resp.state.offset.y;
                 scroll_resp.inner
             });
 
         if self.session.is_placeholder() {
-            ui.data_mut(|d| d.insert_temp(state_id, (DEFAULT_ZOOM, Vec2::splat(0.0))));
-            ui.data_mut(|d| d.insert_temp(scroll_offset_id, Vec2::splat(0.0)));
+            ui.data_mut(|d| d.insert_temp(state_id, ViewPortState::default()));
             return ui.add(StatusPage::status_nothing_open());
         }
 
@@ -112,7 +155,7 @@ impl Widget for TracksView<'_> {
             .frame(Frame::NONE)
             .show_inside(ui, |ui| {
                 ui.style_mut().spacing.item_spacing = Vec2::splat(0.0);
-                ui.set_width(length * zoom);
+                ui.set_width(length * state.time_zoom);
                 let faint_bg_color = ui.global_style().visuals.faint_bg_color;
 
                 let col_major = Color32::from_hex("#7777").unwrap();
@@ -120,15 +163,16 @@ impl Widget for TracksView<'_> {
                 let stroke_major = Stroke::new(1., col_major);
                 let stroke_minor = Stroke::new(1., col_minor);
 
+                let viewport_time_off = vec2(state.time_off, 0.0);
                 let tracks_clip_rect = {
                     let rect = ui.available_rect_before_wrap();
                     rect.with_min_y(rect.min.y + TOP_HEIGHT)
                 };
                 let tracks_painter =
                     Painter::new(ui.ctx().clone(), ui.layer_id(), tracks_clip_rect);
-                let view_time_scale = zoom / midi_time_scale;
-                let tracks_scale = vec2(view_time_scale, TRACK_HEIGHT);
-                let tracks_position = tracks_clip_rect.min - scroll_offset;
+                let view_time_scale = state.time_zoom / midi_time_scale;
+                let tracks_scale = vec2(view_time_scale, state.track_height);
+                let tracks_position = tracks_clip_rect.min - vec2(0.0, state.scroll_off);
                 let tracks_area_size = tracks_clip_rect.size();
 
                 let meter_clip_rect = {
@@ -158,8 +202,8 @@ impl Widget for TracksView<'_> {
                 {
                     let bar_scale = vec2(view_time_scale, tracks_area_size.y);
                     for bar in self.session.beats_paint_cache() {
-                        let a = tracks_clip_rect.min + (bar.0[0] - offset) * bar_scale;
-                        let b = tracks_clip_rect.min + (bar.0[1] - offset) * bar_scale;
+                        let a = tracks_clip_rect.min + (bar.0[0] - viewport_time_off) * bar_scale;
+                        let b = tracks_clip_rect.min + (bar.0[1] - viewport_time_off) * bar_scale;
                         tracks_painter
                             .line(vec![a, b], if bar.1 { stroke_major } else { stroke_minor });
                     }
@@ -168,7 +212,8 @@ impl Widget for TracksView<'_> {
                 // Tracks
                 for (index, track) in self.session.tracks_mut().iter_mut().enumerate() {
                     let track_row_offset = vec2(0.0, index as f32 * tracks_scale.y);
-                    let position = tracks_position - offset * tracks_scale + track_row_offset;
+                    let position =
+                        tracks_position - viewport_time_off * tracks_scale + track_row_offset;
 
                     ui.add(TrackPreview::new(
                         index,
@@ -185,7 +230,7 @@ impl Widget for TracksView<'_> {
                 // Meter Events
                 for (time, event) in self.session.marker_events() {
                     const ANCHOR: Align2 = Align2::LEFT_TOP;
-                    let pos = meter_clip_rect.min - offset * tracks_scale
+                    let pos = meter_clip_rect.min - viewport_time_off * tracks_scale
                         + vec2(*time as f32, 0.0) * tracks_scale;
                     let font_id = FontId::monospace(10.0);
                     match event {
@@ -223,40 +268,30 @@ impl Widget for TracksView<'_> {
                 })
             });
             if let Some((delta, modifiers)) = mouse_wheel {
-                if modifiers.is_none() {
-                    scroll_offset.y -= delta * 50.0;
-                } else if modifiers.ctrl {
-                    let old_zoom = zoom;
-                    let mut new_zoom = old_zoom;
-                    if delta > 0.0 {
-                        new_zoom *= 1.0 + delta * 0.2;
-                    } else {
-                        new_zoom /= 1.0 - delta * 0.2;
-                    }
-
-                    zoom = new_zoom;
-
-                    let old_len = response.rect.width() / old_zoom;
-                    let new_len = response.rect.width() / new_zoom;
-                    let delta_len = new_len - old_len;
-
-                    let relative_cursor_pos =
-                        ui.input(|ui| ui.pointer.hover_pos().unwrap()) - response.rect.min;
-                    offset.x -= delta_len * relative_cursor_pos.x / response.rect.width();
-                } else if modifiers.alt {
-                    offset.x -= delta / zoom * midi_time_scale * 50.;
-                }
-                offset.x = offset.x.clamp(0.0, length);
                 let num_tracks = self.session.tracks().len();
-                let scroll_max = (TOP_HEIGHT + num_tracks as f32 * TRACK_HEIGHT
+                let rect = response.rect;
+                let relative_cursor_pos = ui.input(|ui| ui.pointer.hover_pos().unwrap()) - rect.min;
+
+                if modifiers.is_none() {
+                    state.scroll_off -= delta * 50.0;
+                } else if modifiers.shift_only() {
+                    state.vertical_zoom(delta, num_tracks);
+                } else if modifiers.ctrl {
+                    state.horizontal_zoom(delta, rect, relative_cursor_pos, midi_time_scale);
+                } else if modifiers.alt {
+                    state.time_off -= delta / state.time_zoom * midi_time_scale * 50.;
+                }
+
+                state.time_off = state.time_off.clamp(0.0, length);
+                let scroll_max = (TOP_HEIGHT + num_tracks as f32 * state.track_height
                     - response.rect.height())
                 .max(0.0);
-                scroll_offset.y = scroll_offset.y.clamp(0.0, scroll_max);
+
+                state.scroll_off = state.scroll_off.clamp(0.0, scroll_max);
             }
         }
 
-        ui.data_mut(|d| d.insert_temp(state_id, (zoom, offset)));
-        ui.data_mut(|d| d.insert_temp(scroll_offset_id, scroll_offset));
+        ui.data_mut(|d| d.insert_temp(state_id, state));
 
         response
     }
