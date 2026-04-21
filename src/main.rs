e05cdf9be7fae553c04abe2ec1dff28d89bed71b
sevonj <100710152+sevonj@users.noreply.github.com>
2026-04-21 18:30:14 +0300

    fix: splash size

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@
 fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([640.0, 480.0])
-            .with_min_inner_size([640.0, 480.0]),
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([400.0, 300.0]),
         ..Default::default()
     };
     eframe::run_native(
