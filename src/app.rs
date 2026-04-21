6da60bf02849f617f217aa13fa3fc0cae8e3af24
sevonj <100710152+sevonj@users.noreply.github.com>
2026-04-21 18:43:03 +0300

    fix: dark mode

--- src/app.rs
+++ src/app.rs
@@ -16,7 +16,6 @@ use egui::Image;
 use egui::Layout;
 use egui::Ui;
 use egui::Vec2;
-use egui::Visuals;
 use egui::WidgetText;
 use egui_extras::install_image_loaders;
 use egui_toast::Toast;
@@ -72,9 +71,7 @@ impl Default for MidiFlipperApp {
 
 impl MidiFlipperApp {
     pub fn new(cc: &CreationContext<'_>) -> Self {
-        cc.egui_ctx
-            .global_style_mut(|style| style.visuals = Visuals::dark());
-
+        cc.egui_ctx.set_theme(eframe::egui::Theme::Dark);
         Default::default()
     }
 
