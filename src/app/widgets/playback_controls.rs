a3439bb5599e1d0638a050407f934468217df0d5
sevonj <100710152+sevonj@users.noreply.github.com>
2026-04-29 15:24:48 +0300

    misc: better flip toggle

--- src/app/widgets/playback_controls.rs
+++ src/app/widgets/playback_controls.rs
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 use egui::Button;
+use egui::Color32;
 use egui::Frame;
 use egui::Image;
 use egui::Label;
 use egui::Layout;
 use egui::RichText;
+use egui::Sense;
+use egui::UiBuilder;
 use egui::Vec2;
 use egui::Widget;
 use egui::include_image;
@@ -89,55 +92,135 @@ impl Widget for PlaybackControls<'_> {
                 });
 
             Frame::new()
-                .inner_margin(4.0)
+                .inner_margin(0.0)
                 .fill(fill)
                 .corner_radius(4.0)
                 .show(ui, |ui| {
                     ui.set_height(ui.available_height());
 
-                    ui.vertical(|ui| {
-                        ui.set_width(192.0);
-
-                        ui.add(
-                            Label::new(RichText::new(session.name()).monospace())
-                                .wrap_mode(egui::TextWrapMode::Truncate),
-                        );
-                        ui.horizontal(|ui| {
-                            let position = if session.is_playback_in_progress() {
-                                format_duration(session.playback_position())
-                            } else {
-                                String::from("--:--")
-                            };
-                            let duration = if !session.is_placeholder() {
-                                format_duration(session.playback_duration())
-                            } else {
-                                String::from("--:--")
-                            };
-                            ui.monospace(format!("{position}/{duration}"));
-                        })
-                    });
-
-                    ui.horizontal(|ui| {
-                        ui.spacing_mut().item_spacing.y = 0.0;
-                        ui.spacing_mut().item_spacing.x = 2.0;
-
+                    Frame::new().inner_margin(4.0).show(ui, |ui| {
                         ui.vertical(|ui| {
-                            let original = session.playback_original();
-                            if ui
-                                .selectable_label(!original, RichText::new("FLIP").monospace())
-                                .clicked()
-                            {
-                                session.set_playback_original(!original);
-                            }
-                            if ui
-                                .selectable_label(original, RichText::new("ORIG").monospace())
-                                .clicked()
-                            {
-                                session.set_playback_original(!original);
-                            }
+                            ui.set_width(192.0);
+
+                            ui.add(
+                                Label::new(RichText::new(session.name()).monospace())
+                                    .wrap_mode(egui::TextWrapMode::Truncate),
+                            );
+                            ui.horizontal(|ui| {
+                                let position = if session.is_playback_in_progress() {
+                                    format_duration(session.playback_position())
+                                } else {
+                                    String::from("--:--")
+                                };
+                                let duration = if !session.is_placeholder() {
+                                    format_duration(session.playback_duration())
+                                } else {
+                                    String::from("--:--")
+                                };
+                                ui.monospace(format!("{position}/{duration}"));
+                            })
                         });
-                        ui.image(include_image!("../../../assets/icon_flipswap.svg"));
                     });
+
+                    let mut flipbox_response = ui
+                        .scope_builder(
+                            UiBuilder::new()
+                                .id_salt("flip_toggle")
+                                .sense(Sense::click()),
+                            |ui| {
+                                let response = ui.response();
+
+                                let fill = if response.hovered() {
+                                    ui.style().visuals.widgets.hovered.bg_fill
+                                } else {
+                                    Color32::TRANSPARENT
+                                };
+
+                                Frame::new()
+                                    .inner_margin(2.0)
+                                    .fill(fill)
+                                    .corner_radius(4.0)
+                                    .show(ui, |ui| {
+                                        ui.set_height(ui.available_height());
+
+                                        ui.horizontal(|ui| {
+                                            ui.spacing_mut().item_spacing.y = 0.0;
+                                            ui.spacing_mut().item_spacing.x = 2.0;
+
+                                            ui.vertical(|ui| {
+                                                let style = ui.style();
+
+                                                let frame_weak = Frame::new()
+                                                    .inner_margin(2.)
+                                                    .outer_margin(0.)
+                                                    .corner_radius(2.);
+
+                                                let frame_stronk = Frame::new()
+                                                    .fill(style.visuals.selection.bg_fill)
+                                                    .inner_margin(2.)
+                                                    .outer_margin(0.)
+                                                    .corner_radius(2.);
+
+                                                ui.add_space(1.0);
+
+                                                if session.playback_original() {
+                                                    frame_weak.show(ui, |ui| {
+                                                        ui.add(
+                                                            Label::new(
+                                                                RichText::new("FLIP").monospace(),
+                                                            )
+                                                            .selectable(false),
+                                                        );
+                                                    });
+                                                    frame_stronk.show(ui, |ui| {
+                                                        ui.add(
+                                                            Label::new(
+                                                                RichText::new("ORIG")
+                                                                    .monospace()
+                                                                    .strong(),
+                                                            )
+                                                            .selectable(false),
+                                                        );
+                                                    });
+                                                } else {
+                                                    frame_stronk.show(ui, |ui| {
+                                                        ui.add(
+                                                            Label::new(
+                                                                RichText::new("FLIP")
+                                                                    .monospace()
+                                                                    .strong(),
+                                                            )
+                                                            .selectable(false),
+                                                        );
+                                                    });
+                                                    frame_weak.show(ui, |ui| {
+                                                        ui.add(
+                                                            Label::new(
+                                                                RichText::new("ORIG").monospace(),
+                                                            )
+                                                            .selectable(false),
+                                                        );
+                                                    });
+                                                }
+                                            });
+                                            ui.image(include_image!(
+                                                "../../../assets/icon_flipswap.svg"
+                                            ));
+                                        });
+                                    });
+                            },
+                        )
+                        .response;
+
+                    flipbox_response = if session.playback_original() {
+                        flipbox_response.on_hover_text("Switch to flipped")
+                    } else {
+                        flipbox_response.on_hover_text("Switch to original")
+                    };
+
+                    if flipbox_response.clicked() {
+                        session.set_playback_original(!session.playback_original());
+                    }
                 })
         })
         .response
